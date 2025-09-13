9b2e7517174fddee6610f49c6c12abf740542449
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-13 16:28:18 +0600

    delete coffee item added

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const loaderCoffee = useLoaderData();
 
   return (
-    <>
+    <div className="max-w-11/12 mx-auto">
       <h1 className='text-4xl font-bold text-center py-4'>Hot Cold coffees: {loaderCoffee.length}</h1>
-      <div className="grid md:grid-cols-2 gap-5">
+      <div className="grid md:grid-cols-2 gap-5 ">
         {
         loaderCoffee.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
       }
       </div>
-    </>
+    </div>
   )
 }
 
--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,11 +1,44 @@
 import { FaEdit, FaRegEye } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { Link } from "react-router";
+import Swal from "sweetalert2";
 
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, chef, supplier, taste, category, details, photo } = coffee
+    const { _id, name, chef, supplier, taste, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: "DELETE",
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
-        <div className="card card-side bg-base-100 shadow-sm">
+        <div className="card card-side bg-[#F5F4F1] rounded-xl shadow-sm">
             <figure>
                 <img
                     src={photo}
@@ -19,8 +52,10 @@ const CoffeeCard = ({ coffee }) => {
                 <div className="card-actions justify-end">
                     <div className="join join-vertical space-y-2">
                         <button className="btn text-xl text-white rounded-md bg-[#D2B48C]"><FaRegEye /></button>
-                        <button className="btn rounded-md text-xl text-white bg-[#3C393B]"><FaEdit /></button>
-                        <button className="btn rounded-md text-xl text-white bg-[#EA4744]"><MdDelete /></button>
+                        <Link to={`updateCoffee/${_id}`}>
+                            <button className="btn rounded-md text-xl text-white bg-[#3C393B]"><FaEdit /></button>
+                        </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn rounded-md text-xl text-white bg-[#EA4744]"><MdDelete /></button>
                     </div>
                 </div>
             </div>
