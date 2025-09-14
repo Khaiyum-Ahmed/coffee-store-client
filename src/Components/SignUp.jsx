813186a9f6e7bb6d3119afb03255d396eb23fe0a
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-15 03:40:02 +0600

    Creat a user for the DB

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,26 +1,55 @@
 import { useContext } from "react";
 import Navbar from "./Navbar";
 import { AuthContext } from "../Context/AuthContext";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
-    const {signUp} = useContext(AuthContext);
+    const { signUp } = useContext(AuthContext);
 
-    const handleSignUp = e =>{
+    const handleSignUp = e => {
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const email = formData.get('email');
-        const password = formData.get('password')
-        console.log({email,password});
+        const password = formData.get('password');
+        const name = formData.get('name');
+        const address = formData.get('address');
+        const phone = formData.get('phone');
+        const photo = formData.get('photo');
+
+        const userProfile = { email, name, address, phone, photo }
+        console.log(email, password, userProfile)
 
         // create user 
         signUp(email, password)
-        .then(result =>{
-            console.log(result.user)
-        })
-        .catch(error =>{
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result.user);
+
+                // save profile info in the DB
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(userProfile)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            Swal.fire({
+                                position: "top-end",
+                                icon: "success",
+                                title: "Your Account has been created!",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            form.reset();
+                        }
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     return (
         <div>
@@ -34,12 +63,25 @@ const SignUp = () => {
                         <div className="card-body">
                             <form onSubmit={handleSignUp}>
                                 <fieldset className="fieldset">
+                                    <label className="label">Name</label>
+                                    <input type="text" name="name" className="input" placeholder="Name" />
+
+                                    <label className="label">Address</label>
+                                    <input type="text" name="address" className="input" placeholder="Address" />
+
+                                    <label className="label">Phone Number</label>
+                                    <input type="text" name="phone" className="input" placeholder="Phone Number" />
+
+                                    <label className="label">Photo URL</label>
+                                    <input type="text" name="photo" className="input" placeholder="PhotoURL" />
+
                                     <label className="label">Email</label>
                                     <input type="email" name="email" className="input" placeholder="Email" />
+
                                     <label className="label">Password</label>
                                     <input type="password" className="input" name="password" placeholder="Password" />
                                     <div><a className="link link-hover">Forgot password?</a></div>
-                                    <button className="btn btn-neutral mt-4">SignUP</button>
+                                    <button className="btn btn-neutral mt-4">SignUp</button>
                                 </fieldset>
                             </form>
                         </div>
